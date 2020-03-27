246d2c68868d436a8afa99e38801bddea4d71b89
Joao Ricardo Lima <j.ricardo_lima@outlook.com>
2020-03-27 01:36:40 -0400

    Initializing action as parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -35,12 +36,15 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml",(DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentServices());
+			controller.updateTableView();
+		});
 	}
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x-> {});
 	}
 	
 	@Override
@@ -49,27 +53,7 @@ public class MainViewController implements Initializable{
 		
 	}
 	
-	private synchronized void loadView(String nameView) {
-		
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(nameView));
-			
-			VBox vBox = loader.load();//Janela About
-			
-			Scene mainScene = Main.getMainScene();//Pegando a cena atual(janela atual)
-			VBox mainVbox = (VBox)((ScrollPane) mainScene.getRoot()).getContent();//pegando os elementos da janela main
-			
-			Node mainMenu = mainVbox.getChildren().get(0);//Pegando a posição do menu que está na janela atual
-			mainVbox.getChildren().clear();// limpamdo todos os filhos da janela
-			
-			mainVbox.getChildren().add(mainMenu);//adicionando o menu na próxima janela 
-			mainVbox.getChildren().addAll(vBox.getChildren());//adicionando todos os filhos da proxima janela na janela atual
-		} catch (IOException e) {
-			Alerts.showAlert("IO Exception","Error loading view",e.getMessage(),AlertType.ERROR);
-		}
-	}
-	
-private synchronized void loadView2(String nameView) {
+	private synchronized <T>  void loadView(String nameView,Consumer<T> inicializingAction) {
 		
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(nameView));
@@ -85,10 +69,9 @@ private synchronized void loadView2(String nameView) {
 			mainVbox.getChildren().add(mainMenu);//adicionando o menu na próxima janela 
 			mainVbox.getChildren().addAll(vBox.getChildren());//adicionando todos os filhos da proxima janela na janela atual
 			
-			DepartmentListController controller = loader.getController();
+			T controller = loader.getController();
 			
-			controller.setDepartmentService(new DepartmentServices());
-			controller.updateTableView();
+			inicializingAction.accept(controller);
 		} catch (IOException e) {
 			Alerts.showAlert("IO Exception","Error loading view",e.getMessage(),AlertType.ERROR);
 		}
